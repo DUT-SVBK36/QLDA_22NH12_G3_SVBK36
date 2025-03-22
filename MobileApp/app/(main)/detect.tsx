e5e95356f4595dfcb08c43a58427e581d7eb0b49
*CxC6TdYE92 <huukhoa04@gmail.com>
2025-03-22 10:00:59 +0700

    feat(MobileApp): done HomeScreen, DetectScreen

--- MobileApp/app/(main)/detect.tsx
+++ MobileApp/app/(main)/detect.tsx
@@ -1,23 +1,73 @@
-import BackgroundView from "@/components/layout/BackgroundView";
 import CustomButton from "@/components/ui/CustomButton";
+import { BaseColors } from "@/constants/Colors";
 import SharedAssets from "@/shared/SharedAssets";
-import { Text, View } from "react-native";
+import { Container, Fonts } from "@/shared/SharedStyles";
+import { useState } from "react";
+import { ImageBackground, ScrollView, StyleSheet, Text, View} from "react-native";
 
 
 
 export default function DetectScreen(){
+    const [camera, hasCamera] = useState<boolean>(false);
     return (
-        <BackgroundView
-            customBackground={SharedAssets.Bg}
-        >
-            <View>
-                <Text>Camera</Text>
-            </View>
-            <CustomButton
-                variant="orange"
-                label="Connect"
-                onPress={() => alert('connect')}
+        <>
+            <ImageBackground 
+            source={SharedAssets.Bg}
+            resizeMode="cover"
+            style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+            }}
             />
-        </BackgroundView>
+            <ScrollView style={[
+                Container.base
+
+            ]}
+                contentContainerStyle={[
+                    Container.baseContent,
+                ]}
+            >
+                <Text
+                    style={[
+                        Fonts.h1,
+                        styles.title
+                    ]}
+                >
+                    Detect
+                </Text>
+                <View style={styles.cameraContainer}>
+                    <Text style={{
+                        ...Fonts.body,
+                        color: BaseColors.white,
+
+                    }}>
+                        No camera Feed
+                    </Text>
+                </View>
+                <CustomButton
+                    variant="red"
+                    label="Connect"
+                    onPress={() => alert('connect')}
+                />
+            </ScrollView>
+        </>
     )
 }
+const styles = StyleSheet.create({
+    title: {
+        color: BaseColors.dark_pri,
+        textAlign: "left",
+        width: "100%",
+        marginBottom: 12
+    },
+    cameraContainer: {
+        width: "100%",
+        height: 300,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: BaseColors.dark_pri,
+        borderRadius: 5,
+    }
+
+})
--- MobileApp/app/(main)/index.tsx
+++ MobileApp/app/(main)/index.tsx
@@ -1,21 +1,131 @@
-import { ScrollView, StyleSheet, Text } from "react-native";
+import UserCard from "@/components/ui/UserCard";
+import { BaseColors } from "@/constants/Colors";
+import SharedAssets from "@/shared/SharedAssets";
+import { Container, Fonts } from "@/shared/SharedStyles";
+import { Ionicons } from "@expo/vector-icons";
+import { ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function HomeScreen(){
   return (
-    <ScrollView style={styles.container}
-      contentContainerStyle={styles.content}
+    <>
+    <ImageBackground 
+      source={SharedAssets.Bg}
+      resizeMode="cover"
+      style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+      }}
+    />
+    <ScrollView style={[
+      Container.base
+    ]}
+      contentContainerStyle={[
+        Container.baseContent
+      ]}
     >
-      <Text>Home Screen</Text>
+      {/* Screen label */}
+      <Text
+      style={[
+        Container.title,
+        Fonts.h1Large,
+        styles.titleTint
+      ]}
+      >Home
+      </Text>
+
+      {/* User Card here */}
+      <UserCard 
+
+      />
+      <View style={[
+        styles.container,
+        {
+          padding: 16,
+        }
+      ]}>
+        <Text style={[
+          Fonts.h1Large,
+          styles.tint
+        ]}>
+          50%
+        </Text>
+        <Text style={[
+          Fonts.bodySmall,
+          styles.tint
+        ]}>
+          Best right posture accuracy
+        </Text>
+      </View>
+      <View style={
+        {
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          gap: 16,
+        }
+      }>
+        <TouchableOpacity 
+          style={[
+            styles.container,
+            {
+              backgroundColor: BaseColors.dark_pri,
+              display: "flex",
+              flex: 1,
+              gap: 8,
+            }
+          ]}
+        >
+          <Ionicons name="trophy" size={48} color={BaseColors.white} />
+          <Text style={[
+          Fonts.bodySmall,
+          styles.tint
+          ]}>
+          Leaderboard
+        </Text>
+        </TouchableOpacity>
+        <View style={[
+        styles.container,
+        {
+          alignItems: "flex-start",
+          justifyContent: "center",
+          flex: 2,
+        }
+        ]}>
+          <Text style={[
+            Fonts.h1Large,
+            styles.tint
+          ]}>
+            256
+          </Text>
+          <Text style={[
+            Fonts.bodySmall,
+            styles.tint
+          ]}>
+            streak days
+          </Text>
+      </View>
+      </View>
+
     </ScrollView>
+    
+    </>
   )
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#282c4d',
+  titleTint: {
+    color: BaseColors.dark_pri,
   },
-  content: {
-    alignItems: 'center', 
-    justifyContent: 'center'
+  tint: {
+    color: BaseColors.white,
+  },
+  container: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: BaseColors.dark_pri,
+    alignItems: "center",
+    borderRadius: 5,
+    padding: 16,
   }
 });
