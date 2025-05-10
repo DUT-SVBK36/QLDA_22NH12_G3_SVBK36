b419733be269d1a6a876101dc38ca4848a29ff7a
*CxC6TdYE92 <huukhoa04@gmail.com>
2025-05-10 14:18:58 +0700

    fix: remove unnecessary ui
    fix: remove unnecessary ui

--- MobileApp/constants/config.ts
+++ MobileApp/constants/config.ts
@@ -1,9 +1,9 @@
-const ip = "192.168.248.113:8000";
+const ip = "192.168.111.113:8000";
 
 const config = {
   BASE_URL: `http://${ip}/`,
   API_URL: `http://${ip}/api`,
-  CAMERA_URL: "http://192.168.248.93:81/stream",
+  CAMERA_URL: `http://192.168.111.93:81/stream`,
 
   //Socket configuration
   SOCKET_URL: `ws://${ip}/api/ws`,
