2ef151b5eee89a36283f7b0deefefdb1c29982f7
*CxC6TdYE92 <huukhoa04@gmail.com>
2025-05-31 09:58:54 +0700

    style: chart overflow fixed

--- MobileApp/components/ui/charts/PostureDurationChart.tsx
+++ MobileApp/components/ui/charts/PostureDurationChart.tsx
@@ -172,6 +172,7 @@ const styles = StyleSheet.create({
   chartContainer: {
     alignItems: 'center',
     marginVertical: 16,
+    overflow: 'hidden',
   },
   barLabel: {
     color: BaseColors.dark_pri,
